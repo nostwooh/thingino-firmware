224371d2901a866acceaa4aee627c407e8e6af51
DAB <mrdanielabrown@gmail.com>
2026-09-11 09:15:40 -0400

    Revert "Install HomeKit WebUI plugin"
    This reverts commit 13d48ec1b16d767fd99e9f168398012624ed0989.

--- package/thingino-floodlightd/thingino-floodlightd.mk
+++ package/thingino-floodlightd/thingino-floodlightd.mk
@@ -2,7 +2,6 @@ THINGINO_FLOODLIGHTD_VERSION = 1.0.0
 THINGINO_FLOODLIGHTD_SITE_METHOD = local
 THINGINO_FLOODLIGHTD_SITE = $(THINGINO_FLOODLIGHTD_PKGDIR)
 THINGINO_FLOODLIGHTD_LICENSE = GPL-2.0
-THINGINO_FLOODLIGHTD_DEPENDENCIES += thingino-webui
 
 THINGINO_FLOODLIGHTD_MAKE_OPTS = \
 	CC="$(TARGET_CC)" \
@@ -20,14 +19,6 @@ define THINGINO_FLOODLIGHTD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/files/S96floodlightd $(TARGET_DIR)/etc/init.d/S96floodlightd
 	$(INSTALL) -D -m 0755 $(@D)/files/motion.sh      $(TARGET_DIR)/etc/floodlightd/motion.sh
 	$(INSTALL) -D -m 0644 $(@D)/files/floodlightd.conf $(TARGET_DIR)/etc/floodlightd.conf
-	$(INSTALL) -D -m 0644 $(@D)/files/www/config-homekit.html \
-		$(TARGET_DIR)/var/www/config-homekit.html
-	$(INSTALL) -D -m 0644 $(@D)/files/www/a/config-homekit.js \
-		$(TARGET_DIR)/var/www/a/config-homekit.js
-	$(INSTALL) -D -m 0755 $(@D)/files/www/x/json-config-homekit.cgi \
-		$(TARGET_DIR)/var/www/x/json-config-homekit.cgi
-	$(INSTALL) -D -m 0644 $(@D)/files/homekit.webui.json \
-		$(TARGET_DIR)/var/www/a/plugins/homekit.webui.json
 endef
 
 $(eval $(generic-package))
