3e6a7824a6dc6d5f9042547f06db794a3c0fef8a
NosTwoOh <mrdanielabrown@gmail.com>
2026-09-11 22:42:45 -0400

    Renamed motion-watch

--- package/qrencode/qrencode.mk
+++ package/qrencode/qrencode.mk
@@ -1,5 +1,6 @@
 QRENCODE_VERSION = 4.1.1
-QRENCODE_SITE = https://fukuchi.org/works/qrencode
+QRENCODE_SITE = https://github.com/fukuchi/libqrencode/archive/refs/tags
+QRENCODE_SOURCE = v$(QRENCODE_VERSION).tar.gz
 QRENCODE_LICENSE = LGPL-2.1+
 QRENCODE_LICENSE_FILES = COPYING
 QRENCODE_DEPENDENCIES = host-pkgconf
