abecbe912c80f6c96f52129923ce0d528ab549ef
NosTwoOh <mrdanielabrown@gmail.com>
2026-09-11 22:47:58 -0400

    Renamed motion-watch

--- package/qrencode/qrencode.mk
+++ package/qrencode/qrencode.mk
@@ -3,6 +3,8 @@ QRENCODE_SITE = https://github.com/fukuchi/libqrencode/archive/refs/tags
 QRENCODE_SOURCE = v$(QRENCODE_VERSION).tar.gz
 QRENCODE_LICENSE = LGPL-2.1+
 QRENCODE_LICENSE_FILES = COPYING
-QRENCODE_DEPENDENCIES = host-pkgconf
+QRENCODE_AUTORECONF = YES
+QRENCODE_DEPENDENCIES = host-pkgconf libpng
+QRENCODE_CONF_OPTS = --without-tests --disable-sdltest
 
 $(eval $(autotools-package))
